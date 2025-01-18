b8bc565f562dd591122c6b41f672acfa61d4e392
r3z8 <rezaengplus@gmail.com>
2025-01-18 19:26:42 +0330

    demo folder repetitive removed

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import ElasticNet
 import mlflow
 import mlflow.sklearn
 from pathlib import Path
+import os
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -33,10 +34,15 @@ if __name__ == "__main__":
 
     # Read the wine-quality csv file from local
     data = pd.read_csv("red-wine-quality.csv")
+    # os.mkdir('data/')
+    data.to_csv('data/red-wine-quality.csv', index=False)
+
     # data.to_csv("data/red-wine-quality.csv", index=False)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
+    train.to_csv('data/train.csv', index=False)
+    test.to_csv('data/test.csv', index=False)
 
     # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
@@ -59,7 +65,7 @@ if __name__ == "__main__":
 #         artifact_location=Path.cwd().joinpath('myartifacts').as_uri()
 # )
 #     exp = mlflow.set_experiment(experiment_name="experiment_1")
-    exp = mlflow.set_experiment(experiment_name="experiment_3")
+    exp = mlflow.set_experiment(experiment_name="experiment_5")
     # get_exp = mlflow.get_experiment(exp_id)
 
     # print(f"Name: {get_exp.name}")
@@ -98,6 +104,9 @@ if __name__ == "__main__":
     #     mlflow.log_metric("r2", r2)
     #     mlflow.log_metric("mae", mae)
     #     mlflow.sklearn.log_model(lr, "my_new_model")
+    mlflow.start_run()
+
+    mlflow.set_tag("release.version", "0.1")
 
     lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
     lr.fit(train_x, train_y)
@@ -129,6 +138,11 @@ if __name__ == "__main__":
     mlflow.log_metrics(metrics)
 
     mlflow.sklearn.log_model(lr, "my_new_model_1")
+    # mlflow.log_artifact('red-wine-quality.csv')
+    mlflow.log_artifacts('data/')
+
+    artifact_uri = mlflow.get_artifact_uri()
+    print('The artifact path is:', artifact_uri)
 
     # run = mlflow.active_run()
     # run = mlflow.last_active_run()
