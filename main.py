f5784457fab611d29468d57b05eac50372829d9c
r3z8 <rezaengplus@gmail.com>
2025-01-20 18:55:10 +0330

    mlflow signature infer_signature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import warnings
 import argparse
 import logging
+from inspect import signature
+
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -10,7 +12,7 @@ import mlflow
 import mlflow.sklearn
 from pathlib import Path
 import os
-from mlflow.models.signature import ModelSignature
+from mlflow.models.signature import ModelSignature, infer_signature
 from mlflow.types.schema import Schema, ColSpec
 
 logging.basicConfig(level=logging.WARN)
@@ -98,43 +100,11 @@ if __name__ == "__main__":
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
 
-    input_data = [
-        {"name": "fixed acidity", "type": "double"},
-        {"name": "volatile acidity", "type": "double"},
-        {"name": "citric acid", "type": "double"},
-        {"name": "residual sugar", "type": "double"},
-        {"name": "chlorides", "type": "double"},
-        {"name": "free sulfur dioxide", "type": "double"},
-        {"name": "total sulfur dioxide", "type": "double"},
-        {"name": "density", "type": "double"},
-        {"name": "pH", "type": "double"},
-        {"name": "sulphates", "type": "double"},
-        {"name": "alcohol", "type": "double"},
-        {"name": "quality", "type": "double"}
-    ]
-
-    output_data = [{'type': 'long'}]
-
-    input_schema = Schema([ColSpec(col["type"], col['name']) for col in input_data])
-    output_schema = Schema([ColSpec(col['type']) for col in output_data])
-
-    signature = ModelSignature(inputs=input_schema, outputs=output_schema)
-
+    signature = infer_signature(test_x, predicted_qualities)
     input_example = {
-        "fixed acidity": np.array([7.2, 7.5, 7.0, 6.8, 6.9]),
-        "volatile acidity": np.array([0.35, 0.3, 0.28, 0.38, 0.25]),
-        "citric acid": np.array([0.45, 0.5, 0.55, 0.4, 0.42]),
-        "residual sugar": np.array([8.5, 9.0, 8.2, 7.8, 8.1]),
-        "chlorides": np.array([0.045, 0.04, 0.035, 0.05, 0.042]),
-        "free sulfur dioxide": np.array([30, 35, 40, 28, 32]),
-        "total sulfur dioxide": np.array([120, 125, 130, 115, 110]),
-        "density": np.array([0.997, 0.996, 0.995, 0.998, 0.994]),
-        "pH": np.array([3.2, 3.1, 3.0, 3.3, 3.2]),
-        "sulphates": np.array([0.65, 0.7, 0.68, 0.72, 0.62]),
-        "alcohol": np.array([9.2, 9.5, 9.0, 9.8, 9.4]),
-        "quality": np.array([6, 7, 6, 8, 7])
+        "columns": np.array(test_x.columns),
+        "data": np.array(test_x.values)
     }
-
     mlflow.log_artifact('red-wine-quality.csv')
     mlflow.sklearn.log_model(lr, "model", signature=signature, input_example=input_example)
     artifact_uri = mlflow.get_artifact_uri()
